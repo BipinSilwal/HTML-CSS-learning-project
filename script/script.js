ea2e25ed9c44f54c84db7aaa45fff9a41850edbb
unknown <bpnsilwal12@gmail.com>
2021-11-09 12:35:41 +0545

    Project

---
+++ script/script.js
@@ -0,0 +1,11 @@
+
+
+const btnNavEl = document.querySelector('.btn-mobile-nav');
+const headerEl = document.querySelector('.header');
+
+
+btnNavEl.addEventListener('click', ()=>{
+
+        headerEl.classList.toggle('nav-open');
+
+})
